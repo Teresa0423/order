62c599104329cff84e5a9d50a04ac7cdbebd2ac8
Teresa0423 <teresawang0423>
2022-04-29 16:04:02 +0800

    order

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,9 +3,14 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
+    int shipFeed;
 
-    public Order(int amount, Delivery delivery) {
+    public Order(int amount, Delivery delivery,int shipFeed) {
         this.amount = amount;
         this.delivery = delivery;
+        this.shipFeed = shipFeed;
+    }
+    public int total(){
+        return amount+shipFeed;
     }
 }
